05018a9c04e04720f2ca8165bcf15dc512405a15
Alex Novak <alexleonnovak@gmail.com>
2021-07-26 19:37:50 +0300

    fix filter bug

--- src/redux/contacts/contacts.selectors.js
+++ src/redux/contacts/contacts.selectors.js
@@ -2,8 +2,6 @@ import { createSelector } from '@reduxjs/toolkit';
 
 export const getFilter = state => state.contacts.filter;
 
-// export const getLoading = state => state.contacts.loading;
-
 export const getAllContacts = state => state.contacts.items;
 
 export const getFilteredContacts = createSelector(
@@ -13,7 +11,7 @@ export const getFilteredContacts = createSelector(
 		return contacts.filter(
 			contact =>
 				contact.name.toLowerCase().trim().includes(normalizedFilter) ||
-				contact.phone.toLowerCase().trim().includes(normalizedFilter),
+				contact.number.toLowerCase().trim().includes(normalizedFilter),
 		);
 	},
 );
